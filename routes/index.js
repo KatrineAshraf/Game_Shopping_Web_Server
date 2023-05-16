d8f7db6d6016b78a4e6d8981aab6e435f53430b8
Katrine Ashraf <katieelmasry2002@gmail.com>
2023-05-16 13:01:27 +0300

    made stylesheets external + transactions

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,13 @@ router.get('/customers', (req, res) => {
   
 })
    })
+   router.get('/transactions', (req, res) => {
+    db.query("SELECT * from transactions", function (err, data, fields) {
+      if (err) throw err;
+    res.render('transactions', {data: data})
+  
+})
+   })
 router.get('/login' , (req, res) => {
   res.render("login");
 })
@@ -70,10 +77,6 @@ router.post('/signup',urlencodedParser,(req,res) => {
 
 module.exports = router;
 
-function getCustomers(res){  
-    var sql='SELECT * FROM customers';
-  
-}
 
 function isUser(username, password) {
   var found = false;
@@ -100,77 +103,4 @@ function adduser(username, password, age){
   con.query(sql, function (err, data, fields) {
   if (err) throw err;
 })
-}
-function style(res){
-  res.write(` <style>
-    *{
-    font-family: \'Cinzel\', serif;
-}
-body {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-image: url(\"https://images.unsplash.com/photo-1558244661-d248897f7bc4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVwZWF0aW5nJTIwcGF0dGVybnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60\");
-    background-repeat: repeat-y;
-    background-size: cover;
-}
-form{ 
-    padding: 0px 65px 20px 65px;
-    width:220px;
-    align-self: center;
-    margin-top: 15%;
-    margin-bottom: 55%;
-    border-radius: 22px;
-    text-align: center;
-    background-color: rgba(250, 235, 215, 0.956);
-}
-h2{
-  background-color: white;
-  border-radius:20px;
-  padding: 10px;
-}
-th{
-  color:brown;
-}
-table, th ,td{
-  border: 1px black solid;
-  padding: 10px;
-  text-align: center;
-}
-table{
-  background-color: rgba(250, 235, 215, 0.956);
-  border-radius: 22px;
-  text-align: center;
-  width: 50%;
-}
-}
-span{
-    display: flex;
-    justify-content: space-between;
-}
-h1 hr {
-    margin-top: -5px;
-}
-button{
-border-radius: 15px;
-border: 0.5px solid black;
-font-weight: 500;
-}
-form p {
-    margin-left: -30px;
-    margin-right: -30px;
-}
-input{
-    margin-bottom: 15px;
-    width: 100%;
-    height: 25px;
-    border-radius: 10px;
-    border: 0.5px solid black;
-padding:10px
-}
-a, a:hover, a:visited, a:checked{
-    text-decoration: none;
-  }
-  </style>`)
 }
