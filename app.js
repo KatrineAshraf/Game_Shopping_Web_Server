dcedaf24e848e7d9eb85ce63b691f2b1220a27e2
Katrine Ashraf <96535434+KatrineAshraf@users.noreply.github.com>
2024-07-28 21:29:19 +0300

    Favicon

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const ejs = require('ejs')
+const favicon = require('serve-favicon');
 var bodyParser = require('body-parser')
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/customers');
@@ -14,7 +15,7 @@ app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use(express.static('public'));
 var jsonParser = bodyParser.json()
-
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
